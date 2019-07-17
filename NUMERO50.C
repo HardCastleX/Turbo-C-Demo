3fae6f947ad708d36d0f0bdd34c0f0e3e1708a41
HardCastleX <50705157+HardCastleX@users.noreply.github.com>
2019-07-17 16:14:38 -0500

    Add files via upload

--- NUMERO50.C
+++ NUMERO50.C
@@ -5,14 +5,15 @@
 #include <dos.h>
 
 time_t t;
-
 int x1,x2,x3; //Horizontal
 int y1,y2,y3,y4; // Vertical
 int h5=35;
 int v5=10;
+int comb;
 
 
-int main(){
+int main(){f
+for(comb; comb<=50; comb++){
 	clrscr();
 	srand ((unsigned) time(&t));
 	x1 = rand()%2;
@@ -22,13 +23,18 @@ int main(){
 	y2 = rand()%2;
 	y3 = rand()%2;
 	y4 = rand()%2;
-	gotoxy(31,8); printf("%d %d %d %d %d %d %d",x1,x2,x3,y1,y2,y3,y4);
+	gotoxy(26,10); printf("X1= %d", x1);
+	gotoxy(26,11); printf("X2= %d", x2);
+	gotoxy(26,12); printf("X3= %d", x3);
+	gotoxy(45,10); printf("Y1= %d", y1);
+	gotoxy(45,11); printf("Y2= %d", y2);
+	gotoxy(45,12); printf("Y3= %d", y3);
+	gotoxy(45,13); printf("Y4= %d", y4);
 
 	//X1 Horizontal
 	if(x1==1){
 	for(h5; h5<=40; h5++){
 	gotoxy(h5,10); printf("Û");
-	delay(25);
 	}}
 	h5=35;
 
@@ -36,7 +42,6 @@ int main(){
 	if(x2==1){
 	for(h5; h5<=40; h5++){
 	gotoxy(h5,15); printf("Û");
-	delay(25);
 	}}
 	h5=35;
 
@@ -44,40 +49,36 @@ int main(){
 	if(x3==1){
 	for(h5; h5<=40; h5++){
 	gotoxy(h5,20); printf("Û");
-	delay(25);
 	}}
 
 	//Y1 Vertical
 	if(y1==1){
 	for(v5; v5<=15; v5++){
 	gotoxy(35,v5); printf("Û");
-	delay(25);
 	}}
 	v5=10;
 
 	//Y2 Vertical
-	if(y2==2){
+	if(y2==1){
 	for(v5; v5<=15; v5++){
 	gotoxy(40,v5); printf("Û");
-	delay(25);
 	}}
-	v5=15;
+	v5=10;
 
 	//Y3 Vertical
 	if(y3==1){
 	for(v5; v5<=20; v5++){
 	gotoxy(35,v5); printf("Û");
-	delay(25);
 	}}
-	v5=15;
+	v5=10;
 
 	//Y4 Vertical
 	if(y4==1){
 	for(v5; v5<=20; v5++){
 	gotoxy(40,v5); printf("Û");
-	delay(25);
 	}}
-
-getch();
+	delay(720);
+	clrscr();
+}
 return 0;
 }
