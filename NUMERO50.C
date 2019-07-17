7aab3b7659621e52916bfa8c9d20350fcfa6e711
HardCastleX <50705157+HardCastleX@users.noreply.github.com>
2019-07-17 04:00:10 -0500

    Add files via upload

---
+++ NUMERO50.C
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <dos.h>
+
+time_t t;
+
+int x1,x2,x3; //Horizontal
+int y1,y2,y3,y4; // Vertical
+int h5=35;
+int v5=10;
+
+
+int main(){
+	clrscr();
+	srand ((unsigned) time(&t));
+	x1 = rand()%2;
+	x2 = rand()%2;
+	x3 = rand()%2;
+	y1 = rand()%2;
+	y2 = rand()%2;
+	y3 = rand()%2;
+	y4 = rand()%2;
+	gotoxy(31,8); printf("%d %d %d %d %d %d %d",x1,x2,x3,y1,y2,y3,y4);
+
+	//X1 Horizontal
+	if(x1==1){
+	for(h5; h5<=40; h5++){
+	gotoxy(h5,10); printf("Û");
+	delay(25);
+	}}
+	h5=35;
+
+	//X2 Horizontal
+	if(x2==1){
+	for(h5; h5<=40; h5++){
+	gotoxy(h5,15); printf("Û");
+	delay(25);
+	}}
+	h5=35;
+
+	//X3 Horizontal
+	if(x3==1){
+	for(h5; h5<=40; h5++){
+	gotoxy(h5,20); printf("Û");
+	delay(25);
+	}}
+
+	//Y1 Vertical
+	if(y1==1){
+	for(v5; v5<=15; v5++){
+	gotoxy(35,v5); printf("Û");
+	delay(25);
+	}}
+	v5=10;
+
+	//Y2 Vertical
+	if(y2==2){
+	for(v5; v5<=15; v5++){
+	gotoxy(40,v5); printf("Û");
+	delay(25);
+	}}
+	v5=15;
+
+	//Y3 Vertical
+	if(y3==1){
+	for(v5; v5<=20; v5++){
+	gotoxy(35,v5); printf("Û");
+	delay(25);
+	}}
+	v5=15;
+
+	//Y4 Vertical
+	if(y4==1){
+	for(v5; v5<=20; v5++){
+	gotoxy(40,v5); printf("Û");
+	delay(25);
+	}}
+
+getch();
+return 0;
+}
